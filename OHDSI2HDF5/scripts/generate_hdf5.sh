38fc1cfe7b012785e580c13810d0264beaf7e40b
janos <risk.limits@gmail.com>
2018-12-19 11:49:20 -0500

    ATC coding adding

--- OHDSI2HDF5/scripts/generate_hdf5.sh
+++ OHDSI2HDF5/scripts/generate_hdf5.sh
@@ -2,7 +2,8 @@
 
 python /root/Git/TransformDBtoHDF5ML/scripts/build_hdf5_matrix_from_document.py -a ohdsi_mapped \
  -b /root/ohdsi/build_hdf5/mapped_ohdsi_cdm_batches.json \
- -c /root/Git/MappingOHDSI2HDF5/mappings/5.2/ohdsi_json_2_hdf5.json \
-    /root/Git/MappingOHDSI2HDF5/mappings/5.2/ohdsi_measurement_values_json_2_hdf5.json \
-    /root/Git/MappingOHDSI2HDF5/mappings/5.2/ohdsi_observation_values_json_2_hdf5.json \
-    /root/Git/MappingOHDSI2HDF5/mappings/5.2/ohdsi_source_codes_json_2_hdf5.json
+ -c /root/Git/MappingOHDSI2HDF5/mappings/5.3/ohdsi_json_2_hdf5.json \
+    /root/Git/MappingOHDSI2HDF5/mappings/5.3/ohdsi_measurement_values_json_2_hdf5.json \
+    /root/Git/MappingOHDSI2HDF5/mappings/5.3/ohdsi_observation_values_json_2_hdf5.json \
+    /root/Git/MappingOHDSI2HDF5/mappings/5.3/ohdsi_source_codes_json_2_hdf5.json \
+    /root/Git/MappingOHDSI2HDF5/mappings/5.3/ohdsi_atc_json_2_hdf5.json
