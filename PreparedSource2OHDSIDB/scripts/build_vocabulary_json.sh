adfba2186a40cfecb0a2ddca7c460833cd8984a6
janos <risk.limits@gmail.com>
2019-01-08 18:26:01 -0500

    ATC coding adding

--- PreparedSource2OHDSIDB/scripts/build_vocabulary_json.sh
+++ PreparedSource2OHDSIDB/scripts/build_vocabulary_json.sh
@@ -6,14 +6,16 @@
 # Unzip vocabulary
 cd /root/ohdsi/vocabulary
 
-wget $(cat ../config/s3.ohdsi_vocab.download.txt)
+rm ohdsi_vocab.zip
+wget $(cat s3.ohdsi_vocab.download.txt)
 
+rm *.csv *.bat readme.txt *.jar *.sh
 unzip ohdsi_vocab.zip
 
 # Run java file to extract out CPT codes
-# Please edit this file in a running docker and put your UMLS credentials here: https://uts.nlm.nih.gov/home.html
-java -Dumls-user=XXXX -Dumls-password=XXXX --add-modules=java.xml.ws -jar cpt4.jar 5
+# Please edit this file in a running docker or set environment variables and put your UMLS credentials here: https://uts.nlm.nih.gov/home.html
+java  -Dumls-user=$UMLS_USER_NAME -Dumls-password=$UMLS_PASSWORD --add-modules=java.xml.ws -jar cpt4.jar 5
 
 # Build JSON
 cd ~/ohdsi
-python3.6 ~/Git/CommonDataModelMapper/omop_cdm/utility_programs/generate_code_lookup_json.py -c ~/ohdsi/config/ps_2_cdm_config.json
+python3 ~/Git/CommonDataModelMapper/omop_cdm/utility_programs/generate_code_lookup_json.py -c ~/ohdsi/config/ps_2_cdm_config.json
