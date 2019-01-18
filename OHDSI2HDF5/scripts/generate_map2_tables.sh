f137718b279ce482a7e29d412e2f58e0b72a30ae
janos <risk.limits@gmail.com>
2019-01-18 14:32:55 -0500

    JAVA issue fixed

--- OHDSI2HDF5/scripts/generate_map2_tables.sh
+++ OHDSI2HDF5/scripts/generate_map2_tables.sh
@@ -1,1 +1,2 @@
+cd /root/Git/MappingOHDSI2HDF5/sql/
 python execute_sql_files.py -f ./5.3/create_denormalized_tables_5_3.sql -c "postgresql+psycopg2://postgres@172.17.0.7:5432/postgres" -s ohdsi_cdm
