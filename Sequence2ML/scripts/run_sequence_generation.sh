eb0091107345c6cae3df5917478889886d55e802
jhajagos <janos.hajagos@stonybrook.edu>
2020-06-07 16:41:20 -0400

    Removed mkdir which is not needed

--- Sequence2ML/scripts/run_sequence_generation.sh
+++ Sequence2ML/scripts/run_sequence_generation.sh
@@ -11,8 +11,6 @@ python3 assemble_sequences.py -j ../ohdsi/config/config_assemble_mapping.json  -
 
 python3 generate_sequences.py -j ../ohdsi/config/config_sequences.json  -i /root/ohdsi/output/ohdsi_assembled.json.txt -o /root/ohdsi/output/ohdsi_sequence.json.txt
 
-mkdir /root/ohdsi/output/csv/
-
 python3 package_sequences.py -f /root/ohdsi/output/ohdsi_sequence.json.txt -c scan -b ohdsi -d /root/ohdsi/output/csv/
 
 python3 package_sequences.py -f /root/ohdsi/output/ohdsi_sequence.json.txt -c csv -b ohdsi -d /root/ohdsi/output/csv/
